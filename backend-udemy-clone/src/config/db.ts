ca764aedbaaedd663afddabd44567c130743029a
Srilatha <dlathasri@gmail.com>
2026-04-10 18:50:52 -0400

    fixed build errors

--- backend-udemy-clone/src/config/db.ts
+++ backend-udemy-clone/src/config/db.ts
@@ -7,7 +7,7 @@ export const connectDB = async (): Promise<void> => {
 	try {
 		await mongoose.connect(env.MONGO_URI ?? "");
 		console.log("MongoDB connected");
-		await seed();
+		// await seed();
 	} catch (error) {
 		console.error("MongoDB connection error:", error);
 		process.exit(1);
